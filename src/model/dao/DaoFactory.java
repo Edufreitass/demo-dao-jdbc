6f0a4e2f62785128ffec5959614740ead4177afb
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-08-20 15:42:49 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,23 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+
+	/*
+	 * Essa classe expõe um método que retorna o tipo da interface, mas internamente
+	 * ela vai instanciar uma implementação, é uma forma de não expor a implementação.
+	 */
+
+	/*
+	 * A interface é uma forma de deixar sua classe dependente apenas da interface e
+	 * não de uma implementação. Desta forma, você pode no futuro trocar a
+	 * implementação sem se preocupar com impactos em outras partes do código, desde
+	 * que a nova implementação atenda à interface.
+	 */
+
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+	}
+
+}
